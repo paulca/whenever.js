d56b7553c056b21cfec887281730e045d47293f1
Paul Campbell <paul@rslw.com>
2011-06-21 16:04:45 +0200

    variables as functions, less recursive

--- whenever.js
+++ whenever.js
@@ -1,5 +1,8 @@
 var whenever = function(element){
   
+  // Decide what element to bind to
+  // if the element has been defined, use that
+  // otherwise, use an anchor containing the text
   var choose_element = function(element){
     if(typeof whenever.definitions[element] === 'string')
     {
@@ -11,61 +14,69 @@ var whenever = function(element){
     }
   };
   
+  // A private object to store the element, the event and the action
   var binding = {
     selector: choose_element(element)
   };
-
-  var chain =  function(){
+  var is, given, then;
+  
+  is = function(event){
+    binding.event = event
     return {
-      is: function(event){
-        binding.event = event;
-        return chain();
-      },
-      given: function(condition){
-        var function_to_apply = function(){
-          if(typeof whenever.conditions[condition] === 'function')
-          {
-            return whenever.conditions[condition];
-          }
-          else
-          {
-            for(var matcher in whenever.conditions)
-            {
-              var match = condition.match(new RegExp(matcher));
-              if(match)
-              {
-                match.shift();
-                return (function(condition_name, args){
-                  return function(){
-                    return whenever.conditions[condition_name].apply(this, args);
-                  };
-                })(matcher, match);
-              }
-            }
-          }
-        };
-        
-        var other_conditions = function(){ return true;};
-        if(typeof binding.condition === 'function')
+      and: given,
+      given: given,
+      then: then
+    }
+  }
+  
+  given = function(condition){
+    var function_to_apply = function(){
+      if(typeof whenever.conditions[condition] === 'function')
+      {
+        return whenever.conditions[condition];
+      }
+      else
+      {
+        for(var matcher in whenever.conditions)
         {
-          other_conditions = binding.condition;
+          var match = condition.match(new RegExp(matcher));
+          if(match)
+          {
+            match.shift();
+            return (function(condition_name, args){
+              return function(){
+                return whenever.conditions[condition_name].apply(this, args);
+              };
+            })(matcher, match);
+          }
         }
-        binding.condition = function(){
-          return function_to_apply().apply(this) && other_conditions();
-        };
-        var out = chain();
-        out.and = out.given;
-        return out;
-      },
-      then: function(action){
-        whenever[binding.event](binding.selector, action, binding.condition);
-        var out = chain();
-        out.and = out.then;
-        return out;
       }
     };
-  };
-  return chain();
+    
+    var other_conditions = function(){ return true; };
+    if(typeof binding.condition === 'function')
+    {
+      other_conditions = binding.condition;
+    }
+    binding.condition = function(){
+      return function_to_apply().apply(this) && other_conditions();
+    };
+    return {
+      and: given,
+      then: then
+    }
+  }
+  
+  then = function(action){
+    whenever[binding.event](binding.selector, action, binding.condition);
+    return {
+      and: then
+    }
+  }
+  
+  return {
+    is: is
+  }
 };
 
 whenever.definer = function(){}
