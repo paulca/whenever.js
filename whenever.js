6ab86f1160de6ac895870fe3720d5342b794e0ef
Paul Campbell <paul@rslw.com>
2011-07-05 13:08:43 +0100

    move some spacing, add some comments

--- whenever.js
+++ whenever.js
@@ -1,182 +1,189 @@
 var whenever = function(element){
+  // Decide what element to bind to
+  // if the element has been defined, use that
+  // otherwise, use an anchor containing the text
+  var choose_element = function(element){
+    if(typeof whenever.definitions[element] === 'string')
+    {
+      return whenever.definitions[element];
+    }
+    else if(typeof whenever.definitions[element] === 'object')
+    {
+      return whenever.definitions[element];
+    }
+    else
+    {
+      return 'a:contains("' + element + '")';
+    }
+  };
   
-      // Decide what element to bind to
-      // if the element has been defined, use that
-      // otherwise, use an anchor containing the text
-      var choose_element = function(element){
-        if(typeof whenever.definitions[element] === 'string')
-        {
-          return whenever.definitions[element];
-        }
-        else if(typeof whenever.definitions[element] === 'object')
-        {
-          return whenever.definitions[element];
-        }
-        else
-        {
-          return 'a:contains("' + element + '")';
-        }
-      };
-      
-      // choose a function from a collection of functions
-      // returns the function to call as the first argument
-      // and the arguments to call it with as the second
-      var choose_function = function(string_to_match, collection){
-        var args = [];
-        var function_to_call;
-        if(typeof collection[string_to_match] === 'function')
-        {
-          function_to_call = collection[string_to_match];
-        }
-        else
-        {
-          for(var potential_match in collection)
-          {
-            var matches = string_to_match.match(new RegExp(potential_match))
-            if(matches)
-            {
-              matches.shift();
-              function_to_call = collection[potential_match];
-              args = matches;
-            }
-          }
-        }
-        if(function_to_call)
-        {
-          return function(){
-            return function_to_call.apply(this, args);
-          }
-        }
-        else
-        {
-          throw("'" + string_to_match + "' was not found");
-        }
-      }
-      
-      // A private object to store the element, the event and the action
-      var binding = {
-        selector: choose_element(element),
-        conditions: [],
-        actions: [],
-        action: undefined
-      };
-      var is, given, then;
-      
-      // store the event and return an object for chaining
-      is = function(event){
-        if(whenever.translations[event])
-        {
-          binding.event = whenever.translations[event]
-        }
-        else
-        {
-          throw(event + ' does not map to an event')
-        }
-        return {
-          and: given,
-          given: given,
-          then: then
-        }
-      }
-      
-      // store a predicate along the chain
-      given = function(condition){
-        
-        binding.conditions.push(condition)
-        
-        // and and then are before we've called then
-        return {
-          and: given,
-          then: then
-        }
-      }
-      
-      // call the binding function with condition as appropriate
-      then = function(action){
-        var function_to_apply, translation;
-        
-        binding.actions.push(action)
-        if(typeof binding.action === 'function')
-        {
-          whenever.unbind_function_to_event(
-            binding.selector,
-            binding.event,
-            binding.action
-            )
-        }
-    
-        function_to_apply = function(){
-          for(var i = 0; i<binding.conditions.length; i++)
-          {
-            if(choose_function(binding.conditions[i], whenever.conditions).apply(this) === false)
-            {
-              return false
-            }
-          }
-          for(var i = 0; i<binding.actions.length; i++)
-          {
-            choose_function(binding.actions[i], whenever.actions).apply(this);
-          }
-        }
-        
-        binding.action = function_to_apply
-        
-        whenever.bind_function_to_event(
-          binding.selector,
-          binding.event,
-          binding.action
-        )
-        
-        // leave out the condition, we only want it for the first one
-        return {
-          and: then
-        }
-      }
-      
-      return {
-        is: is
-      }
-    };
-    
-    whenever.definer = function(){}
-    whenever.definer.prototype.add = function(object){
-      for(var label in object)
+  // choose a function from a collection of functions
+  // returns the function to call as the first argument
+  // and the arguments to call it with as the second
+  var choose_function = function(string_to_match, collection){
+    var args = [];
+    var function_to_call;
+    if(typeof collection[string_to_match] === 'function')
+    {
+      function_to_call = collection[string_to_match];
+    }
+    else
+    {
+      for(var potential_match in collection)
       {
-        this[label] = object[label];
+        var matches = string_to_match.match(new RegExp(potential_match))
+        if(matches)
+        {
+          matches.shift();
+          function_to_call = collection[potential_match];
+          args = matches;
+        }
       }
-    };
+    }
+    if(typeof function_to_call === 'function')
+    {
+      return function(){
+        return function_to_call.apply(this, args);
+      }
+    }
+    else
+    {
+      throw("'" + string_to_match + "' was not found");
+    }
+  }
+  
+  // A private object to store the element, the event and the action
+  var binding = {
+    selector: choose_element(element),
+    conditions: [],
+    actions: [],
+    action: undefined
+  };
+  var is, given, then;
+  
+  // store the event and return an object for chaining
+  is = function(event){
+    if(whenever.translations[event])
+    {
+      binding.event = whenever.translations[event]
+    }
+    else
+    {
+      throw(event + ' does not map to an event')
+    }
+    return {
+      and: given,
+      given: given,
+      then: then
+    }
+  }
+  
+  // store a predicate along the chain
+  given = function(condition){
     
-    whenever.definitions  = new whenever.definer;
-    whenever.actions      = new whenever.definer;
-    whenever.conditions   = new whenever.definer;
+    binding.conditions.push(condition)
     
-    whenever.translations = {
-      'blurred': 'blur',
-      'clicked':'click',
-      'focussed':'focus',
-      'hovered over':'mouseenter',
-      'hovered out of':'mouseout',
-      'loaded': 'load',
-      'ready': 'ready',
-      'submitted':'submit',
-      'changed': 'change'
-    };
+    // and and then are before we've called then
+    return {
+      and: given,
+      then: then
+    }
+  }
+  
+  // call the binding function with condition as appropriate
+  then = function(action){
+    var function_to_apply;
     
-    whenever.unbind_function_to_event = function(selector, event, action){
-      return jQuery(document).ready(function(){
-        jQuery(document).undelegate(selector, event, action);
-      })
+    // add the action to the chain of actions
+    binding.actions.push(action)
+    
+    // unbind the previous 'then' or 'and'
+    if(typeof binding.action === 'function')
+    {
+      whenever.unbind_function_to_event(
+        binding.selector,
+        binding.event,
+        binding.action
+        )
+    }
+
+    // create a function that runs the condition before running the actions
+    function_to_apply = function(){
+      for(var i = 0; i<binding.conditions.length; i++)
+      {
+        if(choose_function(binding.conditions[i], whenever.conditions).apply(this) === false)
+        {
+          return false
+        }
+      }
+      for(var i = 0; i<binding.actions.length; i++)
+      {
+        choose_function(binding.actions[i], whenever.actions).apply(this);
+      }
     }
     
-    whenever.bind_function_to_event = function(selector, event, action){
-      return jQuery(document).ready(function(){
-        if(event === 'ready' || event === 'load')
-        {
-          action.apply(selector)
-        }
-        else
-        {
-          return jQuery(document).delegate(selector, event, action);
-        }
-      });
+    // save the function to check for later
+    binding.action = function_to_apply
+    
+    // bind it!
+    whenever.bind_function_to_event(
+      binding.selector,
+      binding.event,
+      binding.action
+    )
+    
+    // only and left
+    return {
+      and: then
     }
+  }
+  
+  // is follows whenever
+  return {
+    is: is
+  }
+};
+
+// extend object so we can build up definitions more than once
+whenever.definer = function(){}
+whenever.definer.prototype.add = function(object){
+  for(var label in object)
+  {
+    this[label] = object[label];
+  }
+};
+
+whenever.definitions  = new whenever.definer;
+whenever.actions      = new whenever.definer;
+whenever.conditions   = new whenever.definer;
+
+whenever.translations = {
+  'blurred': 'blur',
+  'clicked':'click',
+  'focussed':'focus',
+  'hovered over':'mouseenter',
+  'hovered out of':'mouseout',
+  'loaded': 'load',
+  'ready': 'ready',
+  'submitted':'submit',
+  'changed': 'change'
+};
+
+whenever.unbind_function_to_event = function(selector, event, action){
+  return jQuery(document).ready(function(){
+    jQuery(document).undelegate(selector, event, action);
+  })
+}
+
+whenever.bind_function_to_event = function(selector, event, action){
+  return jQuery(document).ready(function(){
+    if(event === 'ready' || event === 'load')
+    {
+      action.apply(selector)
+    }
+    else
+    {
+      return jQuery(document).delegate(selector, event, action);
+    }
+  });
+}
